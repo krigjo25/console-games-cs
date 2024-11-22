c029347405206b2c5d11d68dd6f34a0db67c0503
krigjo25 <krigjo25@outlook.com>
2024-11-22 14:19:08 +0100

    IC

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Net.Quic;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
 
 namespace games
 {
@@ -10,65 +12,104 @@ namespace games
             Includes base functionality for the games
          */
         // Class members  / Fields
-        protected private int _lives_;
-        protected private int _level_;
-        protected private int _points_;
-        protected private bool _quit_;
+        protected private int __lives__;
+        protected private int __level__;
+        protected private int __points__;
+        protected private bool __quit__;
+        protected private string __botName__ = "";
+        protected private string __gName__ = "";
+        protected private int __compareScore__;
 
 
         //  Properties @rules
+        protected private string BotName
+        {
+            get { return __botName__; }
+            set
+            {
+                string match = "^[a-z][A-Z]$";
+
+                var regex = new Regex(match);
+
+                if (regex.IsMatch(value))
+                {
+                    __botName__ = value;
+                }
+            }
+        }
         protected private int Lives
         {
-            get { return _lives_; } 
+            get { return __lives__; } 
             set 
             { 
                 if (value > -1 )
                 {
-                    _lives_ = value;
+                    __lives__ = value;
                 }
             }
         }
         protected private int Level
         {
-            get { return _level_; }
+            get { return __level__; }
             set
             {
-                if (value > -1)
+                if (value > 0)
                 {
-                    _level_ = value;
+                    __level__ = value;
                 }
             }
         }
 
         protected private int Points
         { 
-            get { return _points_; }
+            get { return __points__; }
             set
             {
                 if (value >= 0 && value < 2)
                 {
-                    _points_ = value;
+                    __points__ = value;
                 }
             }
         
         }
 
+        protected private int compareScore
+        {
+            get { return __compareScore__; }
+            set
+            {
+                int compare = Points + 3;
+                if (Points == compare)
+                {
+                    __compareScore__ = Points;
+                }
+            }
+        }
+        protected private string Gname
+        {
+            get { return __gName__; }
+            set { __gName__ = value; }
+        }
         protected private bool Quit
         {
-            get { return _quit_; }
+            get { return __quit__; }
             set { 
                     //  Ensure that the game should end
-                    if (_lives_ == 0 || value == true)
+                    if (__lives__ == 0 || value == true)
                     { 
-                        _quit_ = value; 
+                        __quit__ = value; 
                     }
                 }
         }
 
         protected private static void Main(string[] arg)
         {
-            //  Initializing classes
+            // Initializing a new class
             Base @base = new Base();
+
+            //  Game Configurations
+            @base.SetLives(3);
+            @base.GenerateName();
 
             Console.Write(@"Type in an Integer to select one of the following Games
     1 - Crocks game
@@ -80,7 +121,15 @@ namespace games
             //  Ensure that the input is a valid integer
             switch (Convert.ToInt32(option))
             {
+                case 2:
+
+                    @base.Gname = "Guess the number";
+                    GuessTheNumber @gtn = new GuessTheNumber;
+                    @gtn.Start();
+                    break;
+
                 case 1:
+                    @base.Gname = "Crockodile";
                     Crocks @crocks = new Crocks();
                     Console.WriteLine("Starting Crocks game");
                     @crocks.Start();
@@ -98,42 +147,68 @@ namespace games
             return;
         }
         
+        protected private dynamic GenerateName()
+        {
+            Base @base = new Base();
+            @base.BotName = RandomizedName();
+            return 0;
+        }
+        
+        protected private static string RandomizedName()
+        {
+            string[] array = new string[]
+            {
+                "Martin", "Terje", "Marie",
+                "Ellie Marie", "Therese", "Rebecka",
+                "Eskil", "Erlend", "Linda"
+            };
+
+            Random @r = new Random();
+
+            return array[r.Next(0, array.Length)];
+        }
+        
         protected private dynamic SetLives(dynamic lives)
         {
-            _lives_ = lives;
+            __lives__ = lives;
             return Lives;
         }
+        
         protected private dynamic DecreaseLives()
         {
-            _lives_--;
+            __lives__--;
 
             return 0;
         }
 
         protected private dynamic IncreaseLevel()
         {
-            _level_ ++;
-            return _level_;
+            Base @base = new Base();
+            // Compare Score with Compare + 3
+            if (@base.Points == @base.compareScore)
+            {
+                __lives__ ++;
+                __level__++;
+                return __level__;
+            }
         }
 
         protected private dynamic IncreaseScore()
         {
-            _points_ ++;
+            __points__ ++;
             return 0;
         }
 
-        
         protected private dynamic Stop()
         {
 
             //  End the game
             if (Lives == 0)
             {
-                _quit_ = true;
+                __quit__ = true;
                 Console.WriteLine($"\n[ @ ] The Game has ended Points achived : {Points}, Level Achived : {Level} [ @ ]\n");
             }
 
-            
             return 0;
         }
     }
@@ -147,8 +222,8 @@ namespace games
         {
             //  Game Configrutations
             SetLives(3);
-            //  Assigning the number of _lives_
-            Console.WriteLine(_lives_);
+            //  Assigning the number of __lives__
+            Console.WriteLine(__lives__);
             //  Randomizing the numbers
             Random r = new Random();
 
@@ -157,8 +232,6 @@ namespace games
             { 
                 '>', '=', '<' 
             };
-
-
 
             //  Welcome the user to the game, give a brief description about the game
             //  and the rules of the game
@@ -170,23 +243,26 @@ Initializing the game");
 
             while (Quit != true)
             {
-                int num = r.Next(1, 100000);
-                int num1 = r.Next(1, 100000);
+                int[] ran_num = new int[]
+                {
+                    r.Next(1, 100000), r.Next(1, 100000)
+                };
 
-                Console.WriteLine($"\n[ @ ] Is {num} Greater than, equal to or less than {num1}?\n");
-                var prompt = Convert.ToChar(value:Console.ReadLine());
 
-                if (num == num1 && prompt.CompareTo(array[1]) == array[1] )
+                Console.WriteLine($"\n[ @ ] Is {ran_num[0]} Greater than, equal to or less than {ran_num[1]}?\n");
+                var prompt = Convert.ToChar(value: Console.ReadLine());
+
+                if (ran_num[0] == ran_num[1] && prompt.CompareTo(array[1]) == array[1] )
                 {
                     Console.WriteLine("\n[ @ ] Congratulation you recieve +1 points\n");
                     IncreaseScore();
                 }
-                else if (num > num1 && prompt.CompareTo(array[0]) == array[0])
+                else if (ran_num[0] > ran_num[1] && prompt.CompareTo(array[0]) == array[0])
                 {
                     Console.WriteLine("\n[ @ ] Congratulation you recieve +1 points\n");
                     IncreaseScore();
                 }
-                else if (num < num1 && prompt.CompareTo(array[2]) == array[2])
+                else if (ran_num[0] < ran_num[1] && prompt.CompareTo(array[2]) == array[2])
                 {
                     Console.WriteLine("\n[ @ ] Congratulation you recieve +1 points [ @ ]\n");
                     IncreaseScore();
@@ -218,6 +294,18 @@ Initializing the game");
     {
         internal dynamic Start()
         {
+
+            // Increase level
+            //  Every three points Increase level
+            // Points == 0
+            //  if points == points+3
+            // Increase level
+            Console.WriteLine($"Greetings my name is {BotName}, and I'm an bot for this {gName}game.\nIf you guess the correct number 3-5 times I'll make it difficultier.\nFor now I would like you to guess a number between 1-10");
+            //  Guess the number
+            while (Quit != true)
+            {
+            }
+            // End
 
             return 0;
         }
